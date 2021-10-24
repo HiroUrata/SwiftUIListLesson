42cf4d73b585667c53d8b649d2f4af042bdf34f0
HiroUrata <1712026@i.softbank.jp>
2021-10-24 19:34:50 +0900

    Qiita投稿分

--- SwiftUIListLesson/ContentView.swift
+++ SwiftUIListLesson/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let listContents = ["Swift","Kotlin","Dart","Python","Ruby","Java","PHP","Go","JavaScript","TypeScript","C#","C++",]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        List{
+            Section(header: Text("Header"), footer: Text("Footer")){
+                
+                ForEach(0..<listContents.count){ number in
+                 
+                    HStack{
+                        
+                        Text(String(number)).padding(.trailing, 130)
+                    Text(listContents[number])
+                        
+                    }
+                    
+                }
+            }
+            
+            Section(header: Text("Header"), footer: Text("Footer")){
+                
+                ForEach(0..<listContents.count){ number in
+                 
+                    HStack{
+                        
+                        Text(String(number)).padding(.trailing, 130)
+                    Text(listContents[number])
+                        
+                    }
+                    
+                }
+            }
+            
+            
+        }.listStyle(InsetGroupedListStyle())
+        
+        
     }
 }
 
